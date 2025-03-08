77c63d5706b71b2d99d33263703fdc818b2e5283
Tim Myers <timmyers09@gmail.com>
2025-03-08 10:13:49 -0700

    chore: tf bootstrapping

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "do_token" {
+  description = "DigitalOcean API token"
+  type        = string
+  sensitive   = true
+}
+
+variable "region" {
+  description = "DigitalOcean region"
+  type        = string
+  default     = "sfo2"
+}
