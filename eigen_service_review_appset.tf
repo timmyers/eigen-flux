503ce44e8b1ad9eeae6dfb326a2eba00a6243608
Tim Myers <timmyers09@gmail.com>
2025-03-08 16:46:33 -0700

    fix: patches

--- eigen_service_review_appset.tf
+++ eigen_service_review_appset.tf
@@ -42,15 +42,11 @@ resource "kubernetes_manifest" "eigen_service_review_appset" {
                     kind = "Ingress"
                     name = "eigen-service-review"
                   }
-                  patch = {
-                    spec = {
-                      rules = [
-                        {
-                          host = "{{branch}}.review-eigen.tmye.me"
-                        }
-                      ]
-                    }
-                  }
+                  patch = <<EOF
+- op: replace
+  path: /spec/rules/0/host
+  value: "{{branch}}.review-eigen.tmye.me"
+EOF
                 }
               ]
             }
