6dd5380b76656e8aad248f81b3cc4c2f3c568bae
Tim Myers <timmyers09@gmail.com>
2025-03-08 14:33:08 -0700

    fix: service manifests

--- eigen_service.tf
+++ eigen_service.tf
@@ -9,90 +9,6 @@ resource "kubernetes_namespace" "eigen_service" {
   depends_on = [digitalocean_kubernetes_cluster.primary]
 }
 
-# Create a Kubernetes ConfigMap with the application manifests
-resource "kubernetes_config_map" "eigen_service_manifests" {
-  metadata {
-    name      = "eigen-service-manifests"
-    namespace = kubernetes_namespace.eigen_service.metadata[0].name
-  }
-
-  data = {
-    "deployment.yaml" = <<-EOT
-      apiVersion: apps/v1
-      kind: Deployment
-      metadata:
-        name: eigen-service
-        namespace: eigen-service
-      spec:
-        replicas: 1
-        selector:
-          matchLabels:
-            app: eigen-service
-        template:
-          metadata:
-            labels:
-              app: eigen-service
-          spec:
-            containers:
-            - name: eigen-service
-              image: ghcr.io/timmyers/eigen-service:1.0.8
-              ports:
-              - containerPort: 3000
-              resources:
-                limits:
-                  cpu: "500m"
-                  memory: "512Mi"
-                requests:
-                  cpu: "100m"
-                  memory: "128Mi"
-    EOT
-    "service.yaml" = <<-EOT
-      apiVersion: v1
-      kind: Service
-      metadata:
-        name: eigen-service
-        namespace: eigen-service
-      spec:
-        selector:
-          app: eigen-service
-        ports:
-        - port: 80
-          targetPort: 3000
-        type: ClusterIP
-    EOT
-    "ingress.yaml" = <<-EOT
-      apiVersion: networking.k8s.io/v1
-      kind: Ingress
-      metadata:
-        name: eigen-service-ingress
-        namespace: eigen-service
-        annotations:
-          cert-manager.io/cluster-issuer: "letsencrypt-prod"
-          nginx.ingress.kubernetes.io/ssl-redirect: "true"
-      spec:
-        ingressClassName: nginx
-        tls:
-        - hosts:
-          - eigen.tmye.me
-          secretName: eigen-service-tls
-        rules:
-        - host: eigen.tmye.me
-          http:
-            paths:
-            - path: /
-              pathType: Prefix
-              backend:
-                service:
-                  name: eigen-service
-                  port:
-                    number: 80
-    EOT
-  }
-
-  depends_on = [kubernetes_namespace.eigen_service]
-}
-
-# Create ArgoCD Application resource
 resource "kubernetes_manifest" "eigen_service_app" {
   manifest = {
     apiVersion = "argoproj.io/v1alpha1"
@@ -104,22 +20,9 @@ resource "kubernetes_manifest" "eigen_service_app" {
     spec = {
       project = "default"
       source = {
-        repoURL        = "https://github.com/ArgoProj/applicationset" # Only needed as a placeholder, we're using the inline manifests
+        repoURL        = "https://github.com/timmyers/eigen-flux"
         targetRevision = "HEAD"
-        chart          = ""
-        plugin = {
-          name = "configmap"
-          env = [
-            {
-              name  = "CONFIGMAP_NAME"
-              value = kubernetes_config_map.eigen_service_manifests.metadata[0].name
-            },
-            {
-              name  = "CONFIGMAP_NAMESPACE"
-              value = kubernetes_namespace.eigen_service.metadata[0].name
-            }
-          ]
-        }
+        path           = "manifests/eigen-service"
       }
       destination = {
         server    = "https://kubernetes.default.svc"
@@ -127,9 +30,9 @@ resource "kubernetes_manifest" "eigen_service_app" {
       }
       syncPolicy = {
         automated = {
-          prune       = true
-          selfHeal    = true
-          allowEmpty  = false
+          prune      = true
+          selfHeal   = true
+          allowEmpty = false
         }
         syncOptions = [
           "CreateNamespace=true"
@@ -139,8 +42,7 @@ resource "kubernetes_manifest" "eigen_service_app" {
   }
 
   depends_on = [
-    helm_release.argocd,
-    kubernetes_config_map.eigen_service_manifests
+    helm_release.argocd
   ]
 }
 
