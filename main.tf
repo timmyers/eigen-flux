52a23e7d64f33237e0ba6cc7e9074a028d084b8e
Tim Myers <timmyers09@gmail.com>
2025-03-08 11:14:06 -0700

    feat: add k8s cluster

--- main.tf
+++ main.tf
@@ -20,3 +20,16 @@ terraform {
 provider "digitalocean" {
   token = var.do_token
 }
+
+# Create a new Kubernetes cluster
+resource "digitalocean_kubernetes_cluster" "primary" {
+  name    = var.cluster_name
+  region  = var.region
+  version = var.kubernetes_version
+
+  node_pool {
+    name       = "${var.cluster_name}-worker-pool"
+    size       = var.node_size
+    node_count = var.node_count
+  }
+}
