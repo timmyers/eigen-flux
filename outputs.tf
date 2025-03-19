a661fa1b76288b0adc35d827c1571615fe0857c3
Tim Myers <timmyers09@gmail.com>
2025-03-19 14:42:49 -0600

    chore: comment tf outputs

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,17 @@
-output "cluster_endpoint" {
-  value     = digitalocean_kubernetes_cluster.primary.endpoint
-  sensitive = true
-}
+# output "cluster_endpoint" {
+#   value     = digitalocean_kubernetes_cluster.primary.endpoint
+#   sensitive = true
+# }
 
-output "cluster_id" {
-  value = digitalocean_kubernetes_cluster.primary.id
-}
+# output "cluster_id" {
+#   value = digitalocean_kubernetes_cluster.primary.id
+# }
 
-output "kubeconfig" {
-  value     = digitalocean_kubernetes_cluster.primary.kube_config[0].raw_config
-  sensitive = true
-}
+# output "kubeconfig" {
+#   value     = digitalocean_kubernetes_cluster.primary.kube_config[0].raw_config
+#   sensitive = true
+# }
 
-output "cluster_status" {
-  value = digitalocean_kubernetes_cluster.primary.status
-}
+# output "cluster_status" {
+#   value = digitalocean_kubernetes_cluster.primary.status
+# }
